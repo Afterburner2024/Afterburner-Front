8606627e1abeb17d555534400aa787b48b8a573c
Stjoo0925 <stjoo0925@gmail.com>
2024-10-18 22:45:56 +0900

    feat: swipe update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { useSelector } from "react-redux";
+import { useSwipeable } from "react-swipeable"; // useSwipeable 훅 사용
 import MainPage from "./pages/MainPage";
 import Introduction from "./pages/Introduction";
 import Functions from "./pages/Functions";
@@ -28,6 +29,7 @@ const routeOrder = [
 
 function App() {
   const location = useLocation();
+  const navigate = useNavigate(); // useNavigate 훅 추가
   const [currentPageIndex, setCurrentPageIndex] = useState(0);
   const [isScrollingDown, setIsScrollingDown] = useState(true);
   const status = useSelector(selectStatus);
@@ -39,9 +41,27 @@ function App() {
     if (deltaY > 0 && currentPageIndex < routeOrder.length - 1) {
       setIsScrollingDown(true); // 아래로 스크롤
       setCurrentPageIndex((prev) => prev + 1);
+      navigate(routeOrder[currentPageIndex + 1].path); // 페이지 전환 추가
     } else if (deltaY < 0 && currentPageIndex > 0) {
       setIsScrollingDown(false); // 위로 스크롤
       setCurrentPageIndex((prev) => prev - 1);
+      navigate(routeOrder[currentPageIndex - 1].path); // 페이지 전환 추가
+    }
+  };
+
+  // 스와이프 위 (다음 페이지)
+  const handleSwipedUp = () => {
+    if (currentPageIndex < routeOrder.length - 1) {
+      setCurrentPageIndex(currentPageIndex + 1);
+      navigate(routeOrder[currentPageIndex + 1].path); // navigate로 페이지 전환
+    }
+  };
+
+  // 스와이프 아래 (이전 페이지)
+  const handleSwipedDown = () => {
+    if (currentPageIndex > 0) {
+      setCurrentPageIndex(currentPageIndex - 1);
+      navigate(routeOrder[currentPageIndex - 1].path); // navigate로 페이지 전환
     }
   };
 
@@ -69,12 +89,18 @@ function App() {
     return undefined;
   }, [status, isPreRendered]);
 
+  const swipeHandlers = useSwipeable({
+    onSwipedUp: handleSwipedUp,
+    onSwipedDown: handleSwipedDown,
+    trackMouse: true, // 마우스 드래그로도 테스트 가능
+  });
+
   if (isPreRendered) {
     return <Loading />;
   }
 
   return (
-    <div className="app-container">
+    <div {...swipeHandlers} className="app-container">
       <WheelNavigation />
       <Header />
       {location.pathname !== "/" ? <Navigation /> : null}
