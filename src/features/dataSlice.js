24ade34103bd72636420b8caf3882dfbcfe794c6
Stjoo0925 <stjoo0925@gmail.com>
2024-10-20 01:48:33 +0900

    fix: localstorage updated

--- src/features/dataSlice.js
+++ src/features/dataSlice.js
@@ -2,6 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
 import fetchAllData from "../actions/dataActions";
 
+// 시간 조건 설정 (예: 3시간)
+const DATA_FETCH_INTERVAL = 2 * 60 * 60 * 1000;
+
 // 초기 상태 및 리듀서 정의
 const dataSlice = createSlice({
   name: "data",
@@ -14,6 +17,7 @@ const dataSlice = createSlice({
     contributor: [],
     status: "idle",
     error: null,
+    timestamp: null,
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -29,6 +33,7 @@ const dataSlice = createSlice({
         state.main = action.payload.main || [];
         state.reviews = action.payload.reviews || [];
         state.contributor = action.payload.contributor || [];
+        state.timestamp = Date.now();
       })
       .addCase(fetchAllData.rejected, (state, action) => {
         state.status = "failed";
@@ -40,7 +45,19 @@ const dataSlice = createSlice({
 // 상태에서 필요한 데이터를 선택하는 기본 선택자
 const selectDataState = (state) => state.data;
 
-// selectAllData 선택자를 메모이제이션하여 반환
+// timestamp와 데이터를 확인하는 함수 추가
+export const shouldFetchData = createSelector(
+  [selectDataState],
+  (dataState) => {
+    const currentTime = Date.now();
+    const lastFetched = dataState.timestamp;
+
+    // lastFetched가 없거나 (데이터가 처음 로드되었거나) 일정 시간이 지났으면 true 반환
+    return !lastFetched || currentTime - lastFetched > DATA_FETCH_INTERVAL;
+  },
+);
+
+// selectAllData 선택자를 메모리제이션하여 반환
 export const selectAllData = createSelector([selectDataState], (dataState) => ({
   afterburner: dataState.afterburner,
   functions: dataState.functions,
